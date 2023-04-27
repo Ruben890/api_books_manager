c45c369f65acf484a2a2e6e567230679639f1e48
Darlin Ruben <chinausto@gmail.com>
2023-04-27 19:39:05 -0400

    userManageJWT002

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
 require('dotenv').config();
 const app = express();
 const cors = require('cors');
-const swaggerUI = require('swagger-ui-express');
+
 const V1Router  = require('./v1/routers/router.js');
+
 
 //**Setting */
 app.set('port', process.env.PORT || 4000);
@@ -14,5 +16,6 @@ app.use(cors());
 
 //** Routes */
 app.use('/api/v1/', V1Router)
+
 
 module.exports = app
