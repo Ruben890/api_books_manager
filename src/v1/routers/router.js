8e3e4a87330acedd7e2d830e4736eeada894b325
Darlin Ruben <chinausto@gmail.com>
2023-04-27 18:32:20 -0400

    userJWT001

--- src/v1/routers/router.js
+++ src/v1/routers/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { RegisterUser, loginUser } = require('../../auth/auth.user.js')
+const { RegisterUser, loginUser } = require('../../controller/auth.user.controllerjs')
 const booksController = require('../../controller/books.controller.js');
 
 router
