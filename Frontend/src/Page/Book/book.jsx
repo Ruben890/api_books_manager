83a096dd6faede3ab74f4af5c72e8d36d9666233
Darlin Ruben <chinausto@gmail.com>
2023-05-09 21:42:41 -0400

    book

--- Frontend/src/Page/Book/book.jsx
+++ Frontend/src/Page/Book/book.jsx
@@ -1,3 +1,9 @@
+import { useEffect } from "react"
+import { useSelector, useDispatch } from 'react-redux';
+import { getOneBook } from "../../services/books/books.sevices";
 export const Book = () => {
+    const books = useSelector(state => state.book.books);
+    const dispatch = useDispatch();
+
 
 }
