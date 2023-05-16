02f466963ff02c03140e8316cd7f759766d49027
Darlin Ruben <chinausto@gmail.com>
2023-05-16 17:35:30 -0400

    createBook003

---
+++ Frontend/src/components/protectRouter.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const IsAuthenticated = ({ redirectTo }) => {
+  const user = useSelector(state => state.user.user.data);
+  return user ? <Outlet /> : <Navigate to={redirectTo} replace />;
+};
+
+export default IsAuthenticated;
