af134c970bba340beaf86142264d3214fc19ec61
Darlin Ruben <chinausto@gmail.com>
2023-05-16 19:43:03 -0400

    mysbooks

--- Frontend/src/components/header/header.jsx
+++ Frontend/src/components/header/header.jsx
@@ -18,7 +18,7 @@ export const Header = () => {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Logo</a>
+                    <Link className="navbar-brand" to='/'>Logo</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
